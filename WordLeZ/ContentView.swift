950c83e13a8a04977843b2dee5b75e4a1f87db31
Zhouyuankun <zykun7525599>
2024-10-06 13:07:10 +0800

    Initial Commit

---
+++ WordLeZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordLeZ
+//
+//  Created by 周源坤 on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordLeZ/WordLeZApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordLeZApp.swift
+//  WordLeZ
+//
+//  Created by 周源坤 on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WordLeZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
